e626bc4e2a2febbd7e5cc91de5cf089038d9325f
Eric Hocking <xaoseric@users.noreply.github.com>
2014-12-27 19:04:28 -0500

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
-for i in "dist/Miro Video Converter.app/Contents/Helpers"/*
+for i in "dist/melodyMe Video Converter.app/Contents/Helpers"/*
 do
     codesign -fs \
-      '3rd Party Mac Developer Application: Participatory Culture Foundation' \
+      '3rd Party Mac Developer Application: PlexiLabs' \
       "${i}"
 done
 
 # Fix up the python framework installation from py2app: it doesn't
 # copy over all the files that constitutes it to be a valid framework.
-pushd "dist/Miro Video Converter.app/Contents/Frameworks/Python.framework"
+pushd "dist/melodyMe Video Converter.app/Contents/Frameworks/Python.framework"
 ln -sf Versions/Current/Python Python
 ln -sf Versions/Current/Resources Resources
 ln -sf 2.7 Versions/Current
 popd
 
 codesign -fs \
-  '3rd Party Mac Developer Application: Participatory Culture Foundation' \
-  "dist/Miro Video Converter.app/Contents/Frameworks/Python.framework/Versions/2.7"
+  '3rd Party Mac Developer Application: PlexiLabs' \
+  "dist/melodyMe Video Converter.app/Contents/Frameworks/Python.framework/Versions/2.7"
 
 codesign -fs \
-  '3rd Party Mac Developer Application: Participatory Culture Foundation' \
-  "dist/Miro Video Converter.app/Contents/Frameworks/Sparkle.framework/Versions/A"
+  '3rd Party Mac Developer Application: Plexilabs' \
+  "dist/melodyMe Video Converter.app/Contents/Frameworks/Sparkle.framework/Versions/A"
 
 codesign -fs \
-  '3rd Party Mac Developer Application: Participatory Culture Foundation' \
-  "dist/Miro Video Converter.app"
+  '3rd Party Mac Developer Application: PlexiLabs' \
+  "dist/melodyMe Video Converter.app"
 
 
